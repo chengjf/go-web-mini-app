fe35f490daceae8c9d9a6064db9fcfc67e3b7732
chengjianfeng <sharpkingdom@gmail.com>
2025-07-27 17:49:11 +0800

    feat: init

---
+++ lib/pages/category/state.dart
@@ -0,0 +1,7 @@
+import 'package:go_web_mini_app/common/entities/entities.dart';
+import 'package:get/get.dart';
+
+class CategoryState {
+  // 新闻翻页
+  RxList<NewsItem> newsList = <NewsItem>[].obs;
+}
